b154e51266d6b5da57e7ed74acd9df69c8872fdb
Elian Garcia <telljua90@gmail.com>
2025-03-09 12:33:07 -0600

    conexion a la base de datps sincrona

--- config/database.js
+++ config/database.js
@@ -2,8 +2,18 @@ import mongoose from "mongoose"
 
 const url = process.env.URI_MONGO
 
-mongoose.connect(url)
-    .then(() => console.log("database conected"))
+// mongoose.connect(url)
+//     .then(() => console.log("database conected"))
+//     .catch(error => console.log(error)
+//     )
 
-    .catch(error => console.log(error)
-    )
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.URI_MONGO)
+        console.log("database connected")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+connectDB()

7363383445f6d8634305775d5928b94f7051dcd6
Elian Garcia <telljua90@gmail.com>
2025-03-10 03:11:06 -0600

    cors y morgan integrados

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import express from "express"
 import "dotenv/config.js"
 import "./config/database.js"
+import cors from "cors"
+import morgan from "morgan"
 
 const server = express()
 
 const PORT = process.env.PORT || 8080
 
-const ready = ()=> console.log("Server ready in port:" +PORT)
+const ready = () => console.log("Server ready in port:" + PORT)
 
-server.get("/",(request,response) => {
-    response.send("Hola, Bienvenido a Express")
-})
+//basic settings
 
-server.get("/adios",(request,response) => {
-    response.send("Adios a Express")
-})
-
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(cors())
+server.use(morgan('dev'))
 server.listen(PORT, ready)
